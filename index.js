34114f489127f081309664baf6724165642fc65b
Santosh Kumar <sntshkmr60@gmail.com>
2018-11-17 01:48:46 +0530

    hello world on port 3000

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("REST API")
+let http = require("http");
+
+let server = http.createServer(function(req, res) {
+    res.end("Hello World!\n");
+});
+
+server.listen(3000, function() {
+    console.log("The server is listenting on port 3000");
+});
