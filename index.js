fa9d5ec77825d6669ca06c4c773b67ecba71c75e
Santosh Kumar <sntshkmr60@gmail.com>
2018-11-17 15:16:13 +0530

    parse the payload

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 let http = require("http");
 let url = require("url");
+let StringDecoder = require("string_decoder").StringDecoder;
 
-let server = http.createServer(function(req, res) {
+let server = http.createServer(function (req, res) {
     // parse the passed url
     let parsedUrl = url.parse(req.url, true);
 
@@ -19,16 +20,29 @@ let server = http.createServer(function(req, res) {
     // get headers
     let headers = req.headers;
 
-    // respond to the request
-    res.end("Hello World!\n");
+    // Get the  payload, if any
+    let decoder = new StringDecoder('utf8');
+    let buffer = '';
+    req.on('data', function (data) {
+        buffer += decoder.write(data);
+    });
+    req.on('end', function () {
+        buffer += decoder.end();
 
-    // spit out what client asked for 
-    console.log("Method:", method,
-                "\nPath:", trimmedPath,
-                "\nHeaders:", headers,
-                "\nQuery:", queryStringObject);
+        // respond to the request
+        res.end("Hello World!\n");
+
+        // spit out what client asked for 
+        console.log("Method:", method,
+            "\nPath:", trimmedPath,
+            "\nHeaders:", headers,
+            "\nQuery:", queryStringObject,
+            "\nPayload:", buffer);
+    });
 });
 
-server.listen(3000, function() {
+
+
+server.listen(3000, function () {
     console.log("The server is listenting on port 3000");
 });
